c65d58ff486e69293832cc5a41999ca201891ebf
Thomas Sihapanya <sihapanya.thomas@gmail.com>
2018-12-17 11:45:04 +0100

    Creating post-list and post components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PostListComponent } from './post-list/post-list.component';
+import { PostComponent } from './post/post.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostListComponent,
+    PostComponent
   ],
   imports: [
     BrowserModule
